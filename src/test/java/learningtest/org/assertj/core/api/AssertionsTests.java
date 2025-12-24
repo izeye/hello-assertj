acc8d791f55ce067ba4bb194b2881ea50c6962d9
Johnny Lim <izeye@naver.com>
2025-12-25 01:06:28 +0900

    Add tests for containsAnyOf()
    See https://github.com/assertj/assertj/issues/4078

--- src/test/java/learningtest/org/assertj/core/api/AssertionsTests.java
+++ src/test/java/learningtest/org/assertj/core/api/AssertionsTests.java
@@ -2,6 +2,10 @@ package learningtest.org.assertj.core.api;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Map.entry;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -14,6 +18,19 @@ class AssertionsTests {
     @Test
     void test() {
         assertThat(42).isEqualTo(42);
+    }
+
+    @Test
+    void map() {
+        Map<SomeType, List<String>> map = Map.of(SomeType.TYPE_A, List.of("a", "aa"));
+        assertThat(map).containsAnyOf(entry(SomeType.TYPE_A, List.of("a", "aa")), entry(SomeType.TYPE_B, List.of("b", "bb")));
+        assertThat(map.keySet()).containsAnyOf(SomeType.TYPE_A, SomeType.TYPE_B);
+    }
+
+    enum SomeType {
+        TYPE_A,
+        TYPE_B,
+        TYPE_C
     }
 
 }
